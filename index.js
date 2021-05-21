24f2ef4a5bf4c57cbb82419a5e93f56612ef8d54
alkovtun <alonakovtun55@gmail.com>
2021-05-21 18:15:37 +0200

    product template

--- index.js
+++ index.js
@@ -74,4 +74,30 @@ $(document).ready(function() {
 
     });
 
+    //product qty section
+    let $qty_up = $(".qty .qty-up");
+    let $qty_down = $(".qty .qty-down");
+    let $input = $(".qty .qty_input");
+
+    //click on qty up
+    $qty_up.click(function(e) {
+        if ($input.val() >= 1 && $input.val() <= 9) {
+            $input.val(function(i, oldval) {
+                return ++oldval;
+            });
+        }
+    });
+
+    //click on qty down
+    $qty_down.click(function(e) {
+        if ($input.val() > 1 && $input.val() <= 10) {
+            $input.val(function(i, oldval) {
+                return --oldval;
+            });
+        }
+    });
+
+
+
+
 });
