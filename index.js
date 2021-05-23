18789ab5dd87625074abe3ce82cd50a6ef81666a
alkovtun <alonakovtun55@gmail.com>
2021-05-23 12:28:02 +0200

    cart page

--- index.js
+++ index.js
@@ -77,10 +77,11 @@ $(document).ready(function() {
     //product qty section
     let $qty_up = $(".qty .qty-up");
     let $qty_down = $(".qty .qty-down");
-    let $input = $(".qty .qty_input");
+    //let $input = $(".qty .qty_input");
 
     //click on qty up
     $qty_up.click(function(e) {
+        let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
         if ($input.val() >= 1 && $input.val() <= 9) {
             $input.val(function(i, oldval) {
                 return ++oldval;
@@ -90,6 +91,7 @@ $(document).ready(function() {
 
     //click on qty down
     $qty_down.click(function(e) {
+        let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
         if ($input.val() > 1 && $input.val() <= 10) {
             $input.val(function(i, oldval) {
                 return --oldval;
